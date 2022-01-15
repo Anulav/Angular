d67504abaf2042594db10fa1f5c7a62939cab50a
Anulav <sharan.anulav@gmail.com>
2022-01-15 21:45:31 +0530

    Reactive Pipe and filters

--- sixth/src/app/key-logger/key-logger.component.ts
+++ sixth/src/app/key-logger/key-logger.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { fromEvent, tap } from 'rxjs';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { filter, fromEvent, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-key-logger',
@@ -8,6 +8,7 @@ import { fromEvent, tap } from 'rxjs';
 })
 export class KeyLoggerComponent implements OnInit {
   keys: string = '';
+  @Input() numeric!: boolean;
   @ViewChild('keyContainer',{static: true}) input!: ElementRef; /* Template reference variables can be queried not only in the template, but also in the class component, using the @ViewChild decorator. The @ViewChild
                                                                    decorator accepts two parameters: the name of the template reference variable and an object with a static property. The static property indicates whether the
                                                                    element that we want to query will be available during component initialization. There are cases, however, where its value is false, such as when using an HTML element with an ngIf directive.
@@ -24,16 +25,47 @@ export class KeyLoggerComponent implements OnInit {
   ngOnInit(): void {
     const logger = fromEvent(this.input.nativeElement, 'keyup');
 
-    logger.subscribe((evt)=>{
-      if(evt instanceof KeyboardEvent){ /* Jugaad!!! ;p */
+    logger.subscribe((evt: any)=>{
+       /* Jugaad!!! ;p */
         this.keys = '';
         this.keys += evt.key;
-      }
-    });
-  }
 
+    });
+
+ /*   logger.pipe(tap(evt =>{         /* pipe is used to combine the operators using commas
+      if(evt instanceof KeyboardEvent)
+          console.log(evt.key);
+    })).subscribe();
+  */
+   logger.pipe(
+    map((evt: any) => {return evt.key.charCodeAt(0)}),
+    filter(code => {
+      if (this.numeric) {
+        return !(code > 31 && (code < 48 || code > 57));
+      }
+      return true;
+    }),
+    tap(digit => console.log(String.fromCharCode(digit)))
+  ).subscribe();
+ /* By using Angular Documentation*/
+
+}
 }
 
 
 
-/* https://stackoverflow.com/questions/47166369/argument-of-type-e-customevent-void-is-not-assignable-to-parameter-of-ty*/
+/* https://stackoverflow.com/questions/47166369/argument-of-type-e-customevent-void-is-not-assignable-to-parameter-of-ty
+
+  Creating observales.
+  1. From values such as numbers using 'of':
+  const value = of{1,2,3};
+  values.subscribe(value => console.log(value));
+  2. From array or promise using 'from':
+  const value = from([1,2,3]);
+  const value = from(this.onComplete()); As given above the onComplete is a promise. We could make a Observable from that.
+
+  Refer https://angular.io/guide/rx-library As there have been quite some changes to the usage of RxJs in Angular.
+
+
+
+*/
