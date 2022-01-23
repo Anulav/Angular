338d69bd49b7ee14bab65be9fa1da708ff50f0fc
Anulav <sharan.anulav@gmail.com>
2022-01-23 17:29:46 +0530

    Controlling access to routes using a guard.

---
+++ seventh/src/app/auth.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  private isAuthenticated: boolean = true; /* In a real-world application, we would delegate the decision of whether a user is
+                                              authenticated or not to a separate Angular service. The service would probably
+                                              check the local storage of the browser or any other means to indicate whether
+                                              the user has already authenticated or not.
+  */
+
+  constructor(private router: Router){
+
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkLogin();
+  }
+
+  private checkLogin(): boolean{    /* We created a separate checkLogin method that handles the logic of whether to allow
+    access to the route or not. The canActivate method calls the checkLogin method
+    and returns its value. It checks the value of the isAuthenticated property,
+    and if it is true, the application can navigate to the specified route. Otherwise, it uses the
+    Router service to navigate to the root path of the Angular app and returns false so that the
+    previous navigation can be canceled.
+    */
+
+    if(this.isAuthenticated)
+     return true;
+    this.router.navigate(['/']);
+    return false;
+  }
+
+}
--- seventh/src/app/heroes/heroes-routing.module.ts
+++ seventh/src/app/heroes/heroes-routing.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth.guard';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 
 const routes: Routes = [
-  {path: 'heroes', component : HeroListComponent,
+  /*{path: 'heroes', component : HeroListComponent,
   children :  [                                       /* We use the children property of a route configuration object to define child routes,
                                                          which contains a list of route configuration objects. Notice also that we removed the word
                                                          hero from the path property of the hero route. We wanted to make it clear that it is a
                                                          child of the heroes route, and it should be accessed using the /heroes/:id path
 
-    */
+    *//*
     {path: ':id', component : HeroDetailComponent}
   ]
-  },
+  }
+  */
+  { path: 'heroes', component : HeroListComponent},
+  { path: 'hero/:id', component: HeroDetailComponent, canActivate: [AuthGuard]},
+
   {path: '', redirectTo: '/heroes', pathMatch: 'full'}, //Route for empty path. It is worth noting that we added the empty route path after all other routes because, as we
   /*                                                    have already learned, the order of the routes is important. We want more specific routes
                                                         before less specific ones*/
