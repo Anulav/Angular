cac1c412663b064f2c9d7573af460581c15015af
Anulav <sharan.anulav@gmail.com>
2022-01-27 09:50:58 +0530

    Dynamic form additions using FormArray

--- eighth/src/app/nested-form/nested-form.component.ts
+++ eighth/src/app/nested-form/nested-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-nested-form',
@@ -17,7 +17,12 @@ export class NestedFormComponent implements OnInit {
       age: new FormControl(['']),
       eyes: new FormControl(['']),
       hair: new FormControl([''])
-    })
+    }),
+    powers : new FormArray([]) /* Add a powers property to the heroDetails form group and set its value to
+                                  an instance of the FormArray class. The constructor of the FormArray
+                                  class accepts a list of FormControl instances as a parameter. For now, the list is
+                                  empty since the hero does not have any powers initially:
+    */
   });
 
   ngOnInit(): void {
@@ -27,6 +32,29 @@ export class NestedFormComponent implements OnInit {
     const control = this.heroDetails.controls;
       console.log(control['realName'].value);
       console.log(this.heroDetails.get('biometricData')?.value['age']);
+      this.printPowers();
+  }
+
+  get powers(): FormArray{ /*
+                              Define a getter property that returns the powers form array and cast it as a
+                              FormArray type. Typecasting here is very important since we want to manipulate
+                              the powers property using standard array methods such as push.
+
+    */
+    return this.heroDetails.controls['powers'] as FormArray;
+  }
+
+  addPower(){ /*
+                Binding the click event of the button element to
+                the newly created addPower method
+  */
+    this.powers.push(new FormControl(''));
+  }
+
+  printPowers(){
+    this.powers.controls.forEach(element =>
+        console.log(element.value)
+      );
   }
 
 }
