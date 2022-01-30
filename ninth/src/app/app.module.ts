df12a1b023be962a62fc77580fa8232850a27a47
Anulav <sharan.anulav@gmail.com>
2022-01-30 16:20:05 +0530

    Autocomplete on MatFormField and addition of MatInputModule

--- ninth/src/app/app.module.ts
+++ ninth/src/app/app.module.ts
@@ -6,16 +6,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormsComponent } from './forms/forms.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ninth/src/app/forms/forms.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+import { Hero } from '../hero.model';
+import { Heroes } from '../heroes';
+
+@Component({
+  selector: 'app-form-controls',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.css']
+})
+export class FormsComponent implements OnInit {
+
+  heroes = Heroes;
+  name = new FormControl('', Validators.required);
+
+  filteredHeroes$?: Observable<Hero[]>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.filteredHeroes$ = this.name.valueChanges /*
+            a form control contains a valueChanges observable property. We can subscribe to that property and
+            get notified when the user types in the input control. As soon as the observable emits a
+            new value, we can filter the heroes array according to that value.
+
+    */
+    .pipe(map(name => this.heroes.filter(hero => hero.name.startsWith(name))));
+  }
+
+}
+
+/*
+  Imp: A FormControl instance can be used as a standalone control without necessarily needing to be inside a FormGroup instance.
+
+*/
