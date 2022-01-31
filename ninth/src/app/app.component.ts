c98e2f152296642867dba1a578eadd15253724e3
Anulav <sharan.anulav@gmail.com>
2022-01-31 09:51:06 +0530

    Configuring the dialog box

--- ninth/src/app/app.component.ts
+++ ninth/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,7 +13,14 @@ export class AppComponent {
   constructor(private dialog: MatDialog){}
 
   showdialog(){
-    this.dialog.open(DialogComponent);
+    this.dialog.open(DialogComponent,{
+      autoFocus: false,     /* Setting the configuration for the dialog box here. The autoFocus property is part of the MatDialogConfig object that contains various
+      options that we can use to configure a dialog, such as setting its width or height. We
+      can also use this object to pass data to our dialog component.
+
+      */
+      data: 'Love Angular 10?'
+    });
   }
 
 }
--- ninth/src/app/dialog/dialog.component.ts
+++ ninth/src/app/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any) { }/*
+  In a real-world scenario, you will probably need to create a reusable component for
+  displaying a dialog in an Angular project. Even better, the component may end up in an
+  Angular library as a package. Therefore, you should configure the dialog component to
+  accept data dynamically, such as the title of the dialog.
+
+  Injecting the MAT_DIALOG_DATA token in the constructor of
+  DialogComponent. The MAT_DIALOG_DATA is not an Angular service, and
+  that's the reason why we cannot inject it normally as we do with services. It is an
+  injection token, and we use the @Inject decorator to inject it.The data variable will contain any data that
+  we pass to the dialog when we call its open method
+
+  */
 
   ngOnInit(): void {
   }
