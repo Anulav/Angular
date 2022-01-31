16fb9d1dc2c885bbf07d61af9bd6eeff277c33a2
Anulav <sharan.anulav@gmail.com>
2022-01-31 10:11:04 +0530

    closing dialog with returned data method 2

--- ninth/src/app/dialog/dialog.component.ts
+++ ninth/src/app/dialog/dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -8,7 +8,7 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class DialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data:any) { }/*
+  constructor(@Inject(MAT_DIALOG_DATA) public data:any, private dialogRef: MatDialogRef<DialogComponent>) { }/*
   In a real-world scenario, you will probably need to create a reusable component for
   displaying a dialog in an Angular project. Even better, the component may end up in an
   Angular library as a package. Therefore, you should configure the dialog component to
@@ -20,9 +20,19 @@ export class DialogComponent implements OnInit {
   injection token, and we use the @Inject decorator to inject it.The data variable will contain any data that
   we pass to the dialog when we call its open method
 
+  For closing dialog,
+  Instead of using the mat-dialog-close directive to close a dialog declaratively,
+  we could use the MatDialogRef service. The MatDialogModule exports the
+  MatDialogRef service that contains a close method that we can use.
+
   */
 
   ngOnInit(): void {
+  }
+
+  closeDialog(data?: boolean){
+    this.dialogRef.close(data);
+    window.alert(data);
   }
 
 }
